86418cf35d13a4ffea968686030bfb8d60b619db
huzhangyang <huzhangyang@github.com>
2014-01-18 16:12:59 +0800

    updated vs project.

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -15,6 +15,6 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     // create the application instance
     AppDelegate app;
     EGLView eglView;
-    eglView.init("TestCPP",900,640);
+    eglView.init("TestCPP",1280,720);
     return Application::getInstance()->run();
 }
