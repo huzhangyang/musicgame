25c82dedc8659e4ddf3bbb01a731a9071f9ea409
qiuluoness <diya.qiuluo@gmail.com>
2014-01-30 10:17:51 +0800

    修改游戏界面tag

--- Classes/Resources.h
+++ Classes/Resources.h
@@ -8,6 +8,7 @@ const int MAINSCENE_HELP = 8;
 const int MAINSCENE_EXIT = 9;
 
 const int GAMESCENE_PAUSE = 5;
-const int GAMESCENE_INFO = 6;
-const int GAMESCENE_BAR = 7;
+const int GAMESCENE_INFO = 7;
+const int GAMESCENE_NUMBER = 8;
+const int GAMESCENE_JUDGE = 9;
 #endif 
