c4c31ae755fca6285fea39b1b30e5046e2a699c3
qiuluoness <diya.qiuluo@gmail.com>
2014-02-03 16:25:07 +0800

    tag

--- Classes/Resources.h
+++ Classes/Resources.h
@@ -11,4 +11,18 @@ const int GAMESCENE_PAUSE = 5;
 const int GAMESCENE_COMBO = 6;
 const int GAMESCENE_JUDGE = 7;
 const int GAMESCENE_INFO = 8;
+
+const int CLEARSCENE_PERFECT = 3;
+const int CLEARSCENE_GOOD = 4;
+const int CLEARSCENE_MISS = 5;
+const int CLEARSCENE_PNO = 6;
+const int CLEARSCENE_GNO = 7;
+const int CLEARSCENE_MNO = 8;
+const int CLEARSCENE_NOTE = 9;
+const int CLEARSCENE_COMBO = 10;
+const int CLEARSCENE_NNO = 11;
+const int CLEARSCENE_CNO = 12;
+const int CLEARSCENE_COMPLETE = 13;
+const int CLEARSCENE_RETRY = 14;
+const int CLEARSCENE_RETURN = 15;
 #endif 
