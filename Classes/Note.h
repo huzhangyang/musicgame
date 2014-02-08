4877f2d033ed5dfd16bf6dcc24b6719bf18733a4
huzhangyang <huzhangyang@github.com>
2014-02-08 23:25:46 +0800

    brand new note logic!

--- Classes/Note.h
+++ Classes/Note.h
@@ -19,18 +19,18 @@ public:
 	int getDestY();
 	int getLife();
 	int getLifeSpan();
-	int getLifeTouched();
 	void setTouched();
 	bool isTouched();
+	bool isInLifeSpan();
 private:
 	void update(float dt);
 	void initNote(int type, int pos, int des);
 	int type;//0为普通音符，1为长按音符，2为滑动音符
 	int destX,destY;//滑动音符的目的地
 	bool touched;//是否被触摸过
+	bool inLifeSpan;//是否在生命周期内
 	int lifeSpan;//总生命长度
 	int life;//剩余生命长度
-	int lifeTouched;//保持触摸状态的长度
 };
 
 #endif 
