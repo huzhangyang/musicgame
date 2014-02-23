25bbcbf18778bbe69d5a739a51d98042303bcdbe
qiuluoness <diya.qiuluo@gmail.com>
2014-02-23 18:58:58 +0800

    加入退出确认

--- Classes/Global.h
+++ Classes/Global.h
@@ -13,6 +13,9 @@ const int MAINSCENE_SHELF = 12;
 const int MAINSCENE_TABLE = 13;
 const int MAINSCENE_PAPER = 14;
 const int MAINSCENE_CLOCK = 15;
+const int MAINSCENE_CHECKMARK = 3;
+const int MAINSCENE_CROSSMARK = 4;
+
 //”Œœ∑ΩÁ√ÊTag
 const int GAMESCENE_PAUSE = 5;
 const int GAMESCENE_COMBO = 6;
