227cc2b6d8a6405dfd4c37b378a82af58832cea1
qiuluoness <diya.qiuluo@gmail.com>
2014-02-27 17:24:23 +0800

    ~

--- Classes/MainScene.cpp
+++ Classes/MainScene.cpp
@@ -1,11 +1,6 @@
 #include "MainScene.h"
 #include "GameScene.h"
 #include "MapGenerator.h"
-#include <iostream>
-
-const std::string FILENAME = "test";//曲子文件名
-Node *ExitNode, *dialogNode;
-TextBMFont *labelword;
 
 Scene* MainScene::createScene()
 {
@@ -30,42 +25,43 @@ bool MainScene::init()
 	auto sceneNode = cocostudio::SceneReader::getInstance()->createNodeWithSceneFile("mainScene.json");
 	addChild(sceneNode);
 
-	auto UINode = sceneNode->getChildByTag(10005);
-	auto UIComponent = (cocostudio::ComRender*) UINode->getComponent("mainSceneUI");
-	auto UIlayer = UIComponent->getNode();
-	auto objectTable = dynamic_cast<ImageView*>(UIlayer->getChildByTag(MAINSCENE_TABLE));
-	objectTable->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto objectPaper = dynamic_cast<Button*>(UIlayer->getChildByTag(MAINSCENE_PAPER));
-	objectPaper->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto objectShelf = dynamic_cast<Button*>(UIlayer->getChildByTag(MAINSCENE_SHELF));
-	objectShelf->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto objectClock = dynamic_cast<ImageView*>(UIlayer->getChildByTag(MAINSCENE_CLOCK));
-	objectClock->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto objectCat = dynamic_cast<ImageView*>(UIlayer->getChildByTag(MAINSCENE_CAT));
-	objectCat->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto buttonOption = dynamic_cast<Button*>(UIlayer->getChildByTag(MAINSCENE_OPTION));
-	buttonOption->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto buttonHelp = dynamic_cast<Button*>(UIlayer->getChildByTag(MAINSCENE_HELP));
-	buttonHelp->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto buttonExit = dynamic_cast<Button*>(UIlayer->getChildByTag(MAINSCENE_EXIT));
-	buttonExit->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto objectCharacter = dynamic_cast<ImageView*>(UIlayer->getChildByTag(MAINSCENE_CHARACTER));
-	objectCharacter->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-
+	UINode = sceneNode->getChildByTag(10005);
 	ExitNode = sceneNode->getChildByTag(10004);
+	DialogNode = sceneNode->getChildByTag(10006);
+	auto UIComponent = (cocostudio::ComRender*) UINode->getComponent("mainSceneUI");
 	auto ExitComponent = (cocostudio::ComRender*) ExitNode->getComponent("exitSelectUI");
-	auto Exitlayer = ExitComponent->getNode();
-	auto buttonCheck = dynamic_cast<Button*>(Exitlayer->getChildByTag(MAINSCENE_CHECKMARK));
+	auto DialogComponent = (cocostudio::ComRender*) DialogNode->getComponent("dialogBoxUI");
+	auto UILayer = UIComponent->getNode();
+	auto ExitLayer = ExitComponent->getNode();
+	auto DialogLayer = DialogComponent->getNode();
+	auto objectTable = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_TABLE));
+	auto objectPaper = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_PAPER));
+	auto objectShelf = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_SHELF));
+	auto objectClock = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_CLOCK));
+	auto objectCat = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_CAT));
+	auto objectCharacter = dynamic_cast<ImageView*>(UILayer->getChildByTag(MAINSCENE_CHARACTER));
+	objectBox = dynamic_cast<ImageView*>(DialogLayer->getChildByTag(MAINSCENE_BOX));
+	buttonOption = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_OPTION));
+	buttonHelp = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_HELP));
+	buttonExit = dynamic_cast<Button*>(UILayer->getChildByTag(MAINSCENE_EXIT));
+	buttonCheck = dynamic_cast<Button*>(ExitLayer->getChildByTag(MAINSCENE_CHECKMARK));
+	buttonCross = dynamic_cast<Button*>(ExitLayer->getChildByTag(MAINSCENE_CROSSMARK));
+	labelWord = dynamic_cast<TextBMFont*>(DialogLayer->getChildByTag(MAINSCENE_WORD));
+	objectTable->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	objectPaper->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	objectShelf->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	objectClock->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	objectCat->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	objectCharacter->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	buttonOption->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	buttonHelp->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
+	buttonExit->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
 	buttonCheck->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-	auto buttonCross = dynamic_cast<Button*>(Exitlayer->getChildByTag(MAINSCENE_CROSSMARK));
 	buttonCross->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-
-	dialogNode = sceneNode->getChildByTag(10006);
-	auto dialogComponent = (cocostudio::ComRender*) dialogNode->getComponent("dialogBoxUI");
-	auto dialoglayer = dialogComponent->getNode();
-	auto objectBox = dynamic_cast<ImageView*>(dialoglayer->getChildByTag(MAINSCENE_BOX));
 	objectBox->addTouchEventListener(this, toucheventselector(MainScene::touchEvent));
-
+	objectBox->setEnabled(false);
+	buttonCheck->setEnabled(false);
+	buttonCross->setEnabled(false);
 	return true;
 }
 
@@ -92,64 +88,77 @@ void MainScene::menuCloseCallback(Object* pSender)
 #endif
 }
 
-void MainScene::speak (std::string content)
+void MainScene::createDialog(std::string key)
 {
+	ValueMap strings = FileUtils::getInstance()->getValueMapFromFile("fonts/strings.xml");
+	DialogNode->setVisible(true);
+	objectBox->setEnabled(true);
+	labelWord->setText(strings.at(key).asString().c_str());
 }
 
 void MainScene::touchEvent(Object* obj, gui::TouchEventType eventType)
 {
-	auto button = dynamic_cast<Widget*>(obj);
-	int tag = button->getTag();
-	switch (eventType)
+	auto widget = dynamic_cast<Widget*>(obj);
+	int tag = widget->getTag();
+	std::string musicname = "music/" + FILENAME + ".mp3";
+	std::string mapname = FileUtils::getInstance()->getWritablePath() + FILENAME + ".gnm";
+	if (eventType == TouchEventType::TOUCH_EVENT_ENDED)
 	{
-	case TouchEventType::TOUCH_EVENT_ENDED:
-		if (tag == MAINSCENE_TABLE)
+		switch (tag)
 		{
-		}
-		else if (tag == MAINSCENE_PAPER)
-		{
-			std::string musicname = "music/" + FILENAME + ".mp3";
+		case MAINSCENE_TABLE:
+			this->createDialog("dialogTable");
+			break;
+		case MAINSCENE_PAPER:
 			MapGenerator::generateMap(musicname.c_str());
-		}
-		else if (tag == MAINSCENE_SHELF)
-		{
-			auto scene = GameScene::createScene(FILENAME);
-			Director::getInstance()->replaceScene(TransitionFade::create(2, scene));
-		}
-		else if (tag == MAINSCENE_CLOCK)
-		{
-		}
-		else if (tag == MAINSCENE_CAT)
-		{
-		}
-		else if (tag == MAINSCENE_OPTION)
-		{
+			this->createDialog("dialogMapCreated");
+			break;
+		case MAINSCENE_SHELF:
+			if (FileUtils::getInstance()->isFileExist(mapname))
+			{
+				auto scene = GameScene::createScene(FILENAME);
+				Director::getInstance()->replaceScene(TransitionFade::create(2, scene));
+			}
+			else
+				this->createDialog("dialogNoMap");
+			break;
+		case MAINSCENE_CLOCK:
+			this->createDialog("dialogClock");
+			break;
+		case MAINSCENE_CAT:
+			this->createDialog("dialogCat");
+			break;
+		case MAINSCENE_OPTION:
 			UserDefault::getInstance()->setIntegerForKey("difficulty", 0);
-		}
-		else if (tag == MAINSCENE_HELP)
-		{
-		}
-		else if (tag == MAINSCENE_EXIT)
-		{
+			break;
+		case MAINSCENE_HELP:
+			break;
+		case MAINSCENE_EXIT:
 			ExitNode->setVisible(true);
-		}
-		else if (tag == MAINSCENE_CHECKMARK && ExitNode->isVisible())
-		{
+			buttonOption->setTouchEnabled(false);
+			buttonHelp->setTouchEnabled(false);
+			buttonExit->setTouchEnabled(false);
+			buttonCheck->setEnabled(true);
+			buttonCross->setEnabled(true);
+			break;
+		case MAINSCENE_CHECKMARK:
 			Director::getInstance()->popScene();
-		}
-		else if (tag == MAINSCENE_CROSSMARK && ExitNode->isVisible())
-		{
+			break;
+		case MAINSCENE_CROSSMARK:
 			ExitNode->setVisible(false);
+			buttonOption->setTouchEnabled(true);
+			buttonHelp->setTouchEnabled(true);
+			buttonExit->setTouchEnabled(true);
+			buttonCheck->setEnabled(false);
+			buttonCross->setEnabled(false);
+			break;
+		case MAINSCENE_CHARACTER:
+			this->createDialog("dialogCharacter");
+			break;
+		case MAINSCENE_BOX:
+			DialogNode->setVisible(false);
+			objectBox->setEnabled(false);
+			break;
 		}
-		else if (tag == MAINSCENE_CHARACTER)
-		{
-			dialogNode->setVisible(true);
-		}
-		else if (tag == MAINSCENE_BOX)
-		{
-			dialogNode->setVisible(false);
-		}
-		break;
 	}
 }
-
