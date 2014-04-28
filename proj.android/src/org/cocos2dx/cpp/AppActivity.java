4d05ea3d3df31e3555b4165718b5b959fce09968
huzhangyang <huzhangyang@github.com>
2014-04-28 18:24:17 +0800

    fix bug caused by opengl on android.

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -27,8 +27,14 @@ THE SOFTWARE.
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 public class AppActivity extends Cocos2dxActivity {
+	public Cocos2dxGLSurfaceView onCreateView() {   
+        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);  
+        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);  
+        return glSurfaceView;  
+    } 
 	static {
 		System.loadLibrary("fmodex");
 	}
