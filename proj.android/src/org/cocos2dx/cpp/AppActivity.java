495fb3f7175e2413d9077a814fba884ded8b1eef
huzhangyang <huzhangyang@github.com>
2014-05-24 16:51:49 +0800

    temporarily fix android build.

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -27,14 +27,8 @@ THE SOFTWARE.
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
-import org.cocos2dx.lib.Cocos2dxGLSurfaceView;
 
 public class AppActivity extends Cocos2dxActivity {
-	public Cocos2dxGLSurfaceView onCreateView() {   
-        Cocos2dxGLSurfaceView glSurfaceView = new Cocos2dxGLSurfaceView(this);  
-        glSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 8);  
-        return glSurfaceView;  
-    } 
 	static {
 		System.loadLibrary("fmodex");
 	}
--- proj.android/src/org/cocos2dx/lib/Cocos2dxActivity.java
+++ proj.android/src/org/cocos2dx/lib/Cocos2dxActivity.java
@@ -64,8 +64,9 @@ public abstract class Cocos2dxActivity extends Activity implements Cocos2dxHelpe
 			ApplicationInfo ai = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
 			Bundle bundle = ai.metaData;
 			try {
-        		String libName = bundle.getString("android.app.lib_name");
-        		System.loadLibrary(libName);
+        		//String libName = bundle.getString("android.app.lib_name");
+        		//System.loadLibrary(libName);
+        		System.loadLibrary("musicgame");
 			} catch (Exception e) {
 		 		// ERROR
 				e.printStackTrace();
